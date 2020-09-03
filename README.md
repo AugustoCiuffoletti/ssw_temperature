f813b52168cbc0cbff6a6badefb2e501f3286df9
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-09-03 18:58:54 +0200

    Primo commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lwskkw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-lwskkw)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>Polimorfismo</h1>`;
+
+function display() {
+  var request = new XMLHttpRequest();
+  var city = event.srcElement.id;
+  request.open(
+    "GET",
+    "http://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" +
+      event.srcElement.id,
+    false
+  );
+  request.onload = () => {
+    // funzione definita arrow
+    if (request.status === 200) {
+      var dataObject = JSON.parse(request.response);
+      document.getElementById("risposta").innerText =
+        "A " + city + " ci sono " + dataObject.main.temp + " gradi";
+    } else {
+      document.getElementById("risposta").innerText = "Errore";
+    }
+  };
+  request.send();
+}
