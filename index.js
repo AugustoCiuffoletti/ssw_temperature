bf2cbb079dfeca93e5ca825965fc999c639adbb9
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-11 16:55:58 +0100

    Accenti incompatibili in LaTeX

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const cityElems = Array.from(document.getElementsByClassName('città'));
 // Usate questa per qualche prova, poi create un vostro account
 // su www.openweathermap.org e create una API key personale
 const apiKey = 'd0475be3a1967b1b49dfc02c8128001a';
-const leCittà = ['Genova', 'Milano', 'Torino', 'Roma'];
+const leCitta = ['Genova', 'Milano', 'Torino', 'Roma'];
 const URL =
   'https://api.openweathermap.org/data/2.5/weather?APPID=' +
   apiKey +
   '&units=metric&q=';
 let media = 0;
-// Crea una lista di bottoni con i nomi delle città
-leCittà.map((città) => {
+// Crea una lista di bottoni con i nomi delle citta
+leCitta.map((citta) => {
   const btn = document.createElement('button');
-  btn.innerHTML = città;
+  btn.innerHTML = citta;
   btn.addEventListener('click', () => display(btn.innerHTML));
   const item = document.createElement('li');
   item.appendChild(btn);
-  document.getElementById('città').appendChild(item);
+  document.getElementById('citta').appendChild(item);
 });
 document
   .getElementById('calcolaMedia')
@@ -51,16 +51,16 @@ function display(c) {
 //
 function calcoloMedia() {
   media = 0;
-  leCittà.map((città) => {
-    console.log(città);
+  leCitta.map((citta) => {
+    console.log(citta);
     let request = new XMLHttpRequest();
     request.onload = () => {
       if (request.status === 200) {
-        media += JSON.parse(request.response).main.temp / leCittà.length;
+        media += JSON.parse(request.response).main.temp / leCitta.length;
         document.getElementById('media').innerHTML = media;
       }
     };
-    request.open('GET', URL + città, true);
+    request.open('GET', URL + citta, true);
     request.send();
   });
 }
